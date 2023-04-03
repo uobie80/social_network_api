853d8d66327aba802297775183ca53fa38d65750
Uchenna Obicheta <uobicheta@hotmail.com>
2023-04-02 23:07:28 -0400

    Updated routes. Created controller files.

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/FriendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router;
